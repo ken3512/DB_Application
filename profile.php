402c8594d0e8a7e00eaeb0e35f626e340643fbe9
ken3512 <89816670+ken3512@users.noreply.github.com>
2022-04-11 02:00:40 -0400

    Bruh

--- profile.php
+++ profile.php
@@ -9,9 +9,9 @@
     if (isset($_SESSION["ID"])) {
         $userInfo = getUserInfoById($_SESSION["ID"]);
         echo '<div class="event"><h2 class="pageTitle">' . $userInfo["Name"] . '\'s Profile Page</h2><br>';
-        echo '<p class="desc">Email:'. $userInfo["Gmail"] . '</p><br>';
+        echo '<p class="desc">Email: '. $userInfo["Gmail"] . '</p><br>';
         echo '<p class="desc">University: ' . getUserUniversityName($userInfo["UniversityID"]) . '</p><br>';
-        echo '<p class="desc">User Status:' . getMaxUserStatus($userInfo["ID"]) . "</p><br></div>";
+        echo '<p class="desc">User Status: ' . getMaxUserStatus($userInfo["ID"]) . "</p><br></div>";
         if (isSuperAdmin($_SESSION["ID"])) {
             echo '<h2 class="pageTitle">Approve RSOs for Super Admins:</h2>';
             getUnapprovedRSO($_SESSION["ID"]);
